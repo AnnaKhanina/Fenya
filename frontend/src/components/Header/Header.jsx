beabff5bfe6cec7d1e78cabd98aa91368a3bd39c
AnnaKhanina <khanina.anna85@gmail.com>
2023-04-24 09:19:36 +0300

    test

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -72,17 +72,12 @@
 // export default Header;
 
 
-import { HeaderWrapper, HeaderContainer, Navbar, NavbarLogo, NavbarLinks, NavbarLinksItem, CartLogoBadge, HeaderIconsList, HeaderIconsListItem, HamburgerMenu } from "../Header/Header.styled";
-import { Container } from "../App/App.styled";
 import { Link } from "react-router-dom";
 import React, { useState } from "react";
-
 import { IconContext } from "react-icons";
 import * as FaIcons from "react-icons/fa"; 
 import * as AiIcons from "react-icons/ai";
-import { HiSearch, HiOutlineUser, HiOutlineHeart, HiOutlineShoppingCart, HiOutlineMenu  } from "react-icons/hi";
-import logo from '../../images/logoFenya.png';
-
+import "../Header/Header.css";
 import { SidebarData } from "./SlidebarData";
 
 const Header = () => {
@@ -91,69 +86,36 @@ const Header = () => {
 
     const showSidebar = () => setSidebar(!sidebar);
 
-    return (   
-        <HeaderWrapper>
-            <Container>
-                <HeaderContainer>
-                <IconContext.Provider value={{ color: "#FFF" }}>
-                    <Navbar> 
-                        
-                        <NavbarLogo>                          
-                                <Link to="/" end>                   
-                                    <img src={logo} alt="Logo" />                                     
-                                </Link>                          
-                        </NavbarLogo>
-                        <NavbarLinks>            
-                            <NavbarLinksItem>
-                                <Link to="/products">Товари</Link>
-                            </NavbarLinksItem>
-                            <NavbarLinksItem>
-                                <Link to="/size">Розмірна сітка</Link>
-                            </NavbarLinksItem>
-                            <NavbarLinksItem>
-                                <Link to="/payment_delivery">Оплата і доставка</Link>
-                            </NavbarLinksItem>
-                            <NavbarLinksItem>
-                                <Link to="/contacts">Контакти</Link>
-                            </NavbarLinksItem>
-                        </NavbarLinks>
-                        <HeaderIconsList>
-                            <HeaderIconsListItem>
-                                <div>                               
-                                    <span>
-                                        <HiSearch />                            
-                                            {/* <Input type="text" name="filter" placeholder="Пошук" />                                */}
-                                                {/* <Button type="submit"><HiSearch /></Button> */}
-                                    </span>                      
-                                </div> 
-                            </HeaderIconsListItem>
-                            <HeaderIconsListItem>
-                                <div>                
-                                    <Link to="/profile"><HiOutlineUser /></Link>
-                                </div>
-                            </HeaderIconsListItem>                       
-                            <HeaderIconsListItem>
-                                <div>                
-                                    <Link to="/favorite"><HiOutlineHeart /></Link>
-                                </div>
-                            </HeaderIconsListItem>
-                            <HeaderIconsListItem>
-                                <div>
-                                    <Link to="/cart"><HiOutlineShoppingCart /><span><CartLogoBadge>0</CartLogoBadge></span></Link> 
-                                </div> 
-                            </HeaderIconsListItem>                                         
-                        </HeaderIconsList> 
-                        <HamburgerMenu onClick={click}>                        
-                        <button type="button"><HiOutlineMenu /></button>           
-                    </HamburgerMenu> 
-                    </Navbar>
-                    </IconContext.Provider>
-                    {/* <HamburgerMenu onClick={click}>                        
-                        <button type="button"><HiOutlineMenu /></button>           
-                    </HamburgerMenu>  */}
-                </HeaderContainer> 
-            </Container>                    
-        </HeaderWrapper> 
+    return ( 
+        <>   
+            <IconContext.Provider value={{ color: "#FFF" }}>
+             <div className="navbar">
+                <Link to="#" className="menu-bars">
+                    <FaIcons.FaBars onClick={showSidebar} />
+                </Link>
+                </div>
+                <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+                <ul className="nav-menu-items" onClick={showSidebar}>
+                    <li className="navbar-toggle">
+                    <Link to="#" className="menu-bars">
+                        <AiIcons.AiOutlineClose />
+                    </Link>
+                    </li>
+
+                    {SidebarData.map((item, index) => {
+                    return (
+                        <li key={index} className={item.cName}>
+                        <Link to={item.path}>
+                            {item.icon}
+                            <span>{item.title}</span>
+                        </Link>
+                        </li>
+                    );
+                    })}
+                </ul>
+             </nav>
+            </IconContext.Provider>
+        </>
     )
 };
 
