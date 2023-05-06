ea6fcc635d110864146a446395ec2ddf1b389e4e
Anna Khanina <77342940+AnnaKhanina@users.noreply.github.com>
2023-05-06 10:42:43 +0300

    fix

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -54,34 +54,33 @@ const Header = () => {
                             {/* <Link to="/cart" className="icons-links"><FaIcons.FaCartPlus /><span className="cart-logo-badge">0</span></Link> */}
                                 <Link to="/cart" className="icons-links"><FaIcons.FaCartPlus /></Link> 
                             </li> 
-                        </ul> 
-
-                        <div className="navbar-mob">
-                            <Link to="#" className="menu-bars">
-                                <FaIcons.FaBars onClick={showSidebar} />
-                            </Link>
-                        </div>
-                        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-                            <ul className="nav-menu-items" onClick={showSidebar}>
-                                <li className="navbar-toggle">
-                                    <Link to="#" className="menu-bars">
-                                        <AiIcons.AiOutlineClose />
-                                    </Link>
-                                </li>
-
-                                {SidebarData.map((item, index) => {
-                                return (
-                                    <li key={index} className={item.cName}>
-                                    <Link to={item.path}>
-                                        {item.icon}
-                                        <span>{item.title}</span>
-                                    </Link>
+                        </ul>   
+                            <div className="navbar-mob">
+                                <Link to="#" className="menu-bars">
+                                    <FaIcons.FaBars onClick={showSidebar} />
+                                </Link>
+                            </div>
+                            <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+                                <ul className="nav-menu-items" onClick={showSidebar}>
+                                    <li className="navbar-toggle">
+                                        <Link to="#" className="menu-bars">
+                                            <AiIcons.AiOutlineClose />
+                                        </Link>
                                     </li>
-                                );
-                                })}
-                            </ul>
-                    </nav>
-                </div>
+
+                                    {SidebarData.map((item, index) => {
+                                    return (
+                                        <li key={index} className={item.cName}>
+                                        <Link to={item.path}>
+                                            {item.icon}
+                                            <span>{item.title}</span>
+                                        </Link>
+                                        </li>
+                                    );
+                                    })}
+                                </ul>
+                        </nav>
+                    </div>
             </IconContext.Provider>
         </>
     )
