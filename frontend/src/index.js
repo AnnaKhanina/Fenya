d3a3495daf633cf746d65dfec11905644873a213
AnnaKhanina <khanina.anna85@gmail.com>
2023-05-18 13:51:37 +0300

    productscreen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,7 +13,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Fenya/">
       <App />
     </BrowserRouter>   
   </StrictMode>
--- frontend/src/screens/CartScreen/CartScreen.jsx
+++ frontend/src/screens/CartScreen/CartScreen.jsx
@@ -1,17 +1,20 @@
 import  "../CartScreen/CartScreen.css";
 import "../../components/Container/Container.css";
 import "../MainScreen/MainScreen.css";
+import "../../components/CartItem/CartItem";
 
 const CartScreen = () => {
     return (
             <main className="main-screen">
                 <div className="container">
-                    <h2 className="screen-title">Кошик</h2>
-                    <p className="screen-text">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
-                        laboriosam placeat incidunt rem illum animi nemo quibusdam quia
-                        voluptatum voluptate.
-                    </p> 
+                    <div className="cartscreen-left">
+
+                    </div>
+                    <h2 className="screen-title">Кошик</h2>                    
+                    <CartItem/>
+                    <div className="cartscreen-right">
+                        
+                    </div> 
                 </div>                
             </main>
     );
