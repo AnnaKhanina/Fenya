8cfd6c947124ddf38be5491c81a6cff2146be4bb
Anna Khanina <77342940+AnnaKhanina@users.noreply.github.com>
2023-05-20 17:51:22 +0300

    product

--- frontend/src/screens/ProductScreen/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen/ProductScreen.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+
 import { IconContext } from "react-icons";
 import * as FaIcons from "react-icons/fa"; 
 import "../ProductScreen/ProductScreen.css";
@@ -9,7 +9,7 @@ const ProductScreen = () => {
     return (        
         <main className="main-screen">
             <IconContext.Provider value={{ color: "#060b26" }}> 
-            <div className="container">
+            <div className="container product-container">
                 {/* <h2 className="screen-title">Товари</h2>
                 <p className="screen-text">
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
@@ -32,7 +32,7 @@ const ProductScreen = () => {
                             <p className="product-name">
                                 Наявність: <span className="span-text">є в наявності</span>
                             </p>
-                            <p className="product-name">
+                            <p className="product-name-qty">
                                 Кількість
                                 <select className="product-qty">
                                     <option value="1">1</option>
@@ -40,13 +40,28 @@ const ProductScreen = () => {
                                     <option value="3">3</option>
                                     <option value="4">4</option>
                                 </select>
-                            </p>
-                            <p>
-                                <Link to="/favorite" className="icons-links"><FaIcons.FaHeart /></Link>  
-                            </p>
-                            <p className="product-name">
-                                <button className="product-button" type="button" >Додати в кошик</button> 
-                            </p>                             
+                            </p> 
+                            <ul className="product-icons">
+                                <li className="product-icons-item">
+                                    <button className="product-button" type="button">
+                                        <p className="button-text">купити</p>
+                                    </button> 
+                                </li> 
+                                <li className="product-icons-item">
+                                    <button className="product-favorite-button" type="button" >
+                                        <span className="icon"><FaIcons.FaHeart /></span>
+                                        <p className="icon-text">додати до бажаного</p>
+                                    </button> 
+                                </li>                                   
+                            </ul>                                                     
+                        </li>
+                        <li className="product-list-item">
+                            <p className="product-name">Доставка</p>
+                            <ul className="product-delivery">
+                                <li className="product-delivery-item">Самовивіз — безкоштовно</li>
+                                <li className="product-delivery-item">Адресна доставка по Харкову — 99 грн</li>
+                                <li className="product-delivery-item">Нова пошта — за тарифами перевізника</li>                                
+                            </ul>
                         </li>
                     </ul>                      
                 </div>                   
