b113ddcdc0687fe42e04d4a3d17a8f667672cf41
AnnaKhanina <khanina.anna85@gmail.com>
2023-05-20 13:24:31 +0300

    test

--- frontend/src/screens/ProductScreen/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen/ProductScreen.jsx
@@ -1,51 +1,21 @@
+import { Link } from "react-router-dom";
+import { IconContext } from "react-icons";
+import * as FaIcons from "react-icons/fa"; 
 import "../ProductScreen/ProductScreen.css";
 import "../../components/Container/Container.css";
 import "../MainScreen/MainScreen.css";
 
 const ProductScreen = () => {
-    return (
+    return (        
         <main className="main-screen">
+            <IconContext.Provider value={{ color: "#060b26" }}> 
             <div className="container">
                 {/* <h2 className="screen-title">Товари</h2>
                 <p className="screen-text">
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
                         laboriosam placeat incidunt rem illum animi nemo quibusdam quia
                         voluptatum voluptate.
-                </p> */}
-                {/* <div className="productscreen">
-                    <div className="productscreen-left">
-                        <div className="left-image">
-                        <img src="https://scx1.b-cdn.net/csz/news/800a/2021/cat-1.jpg" alt="жіноча білизна"/>
-                        </div>            
-                        <div className="left-info">
-                            <p className="left-name">Трусики жіночі</p>                          
-                            <p className="left-text">Опис: ажурні трусики від виробника</p>
-                        </div>
-                    </div> 
-                    <div className="productscreen-right">
-                        <div className="right-info">
-                            <p>
-                                Ціна: <span className="right-text">259 грн</span>
-                            </p>
-                            <p>
-                                Наявність: <span className="right-text">є в наявності</span>
-                            </p>
-                            <p>
-                                Кількість
-                                <select className="right-text">
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                </select>
-                            </p>
-                             <p>
-                               <button type="button" >Додати в кошик</button> 
-                            </p>  
-                        </div>
-                    </div>                   
-                </div>  */}
-
+                </p> */}  
                 <div className="productscreen">
                     <ul className="product-list">
                         <li className="product-list-item">
@@ -58,7 +28,7 @@ const ProductScreen = () => {
                         <li className="product-list-item">                          
                             <p className="product-name">
                                 Ціна: <span className="span-text">259 грн</span>
-                            </p>
+                            </p>                            
                             <p className="product-name">
                                 Наявність: <span className="span-text">є в наявності</span>
                             </p>
@@ -72,13 +42,16 @@ const ProductScreen = () => {
                                 </select>
                             </p>
                             <p>
-                            <button className="product-button" type="button" >Додати в кошик</button> 
+                                <Link to="/favorite" className="icons-links"><FaIcons.FaHeart /></Link>  
+                            </p>
+                            <p className="product-name">
+                                <button className="product-button" type="button" >Додати в кошик</button> 
                             </p>                             
                         </li>
                     </ul>                      
                 </div>                   
                 </div>
-      
+            </IconContext.Provider>    
         </main>
     );
 };
