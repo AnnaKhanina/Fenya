eacd88a32f6c8268034d4cf21d8b5333b1d30ade
AnnaKhanina <khanina.anna85@gmail.com>
2023-05-21 11:22:53 +0300

    test color-variants

--- frontend/src/screens/ProductScreen/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen/ProductScreen.jsx
@@ -1,9 +1,10 @@
-
+import { Link } from "react-router-dom";
 import { IconContext } from "react-icons";
 import * as FaIcons from "react-icons/fa"; 
 import "../ProductScreen/ProductScreen.css";
 import "../../components/Container/Container.css";
 import "../MainScreen/MainScreen.css";
+import "../../components/Header/Header.css";
 
 const ProductScreen = () => {
     return (        
@@ -18,7 +19,27 @@ const ProductScreen = () => {
                         </li>
                         <li className="product-list-item">                          
                             <p className="product-name">Трусики ажурні Anna</p>                          
-                            <p className="product-text">Опис: ажурні трусики від виробника</p>                 
+                            <p className="product-text">Опис: ажурні трусики від виробника</p>
+                            <div className="color-variants">
+                                <p className="product-name">Вибір кольору</p>
+                                <ul className="color-list">
+                                    <li className="color-list-item">
+                                        <img className="product-image" src="https://scx1.b-cdn.net/csz/news/800a/2021/cat-1.jpg" alt="жіноча білизна"/>
+                                    </li>
+                                    <li className="color-list-item">
+                                        <img className="product-image" src="https://scx1.b-cdn.net/csz/news/800a/2021/cat-1.jpg" alt="жіноча білизна"/>
+                                    </li>
+                                    <li className="color-list-item">
+                                        <img className="product-image" src="https://scx1.b-cdn.net/csz/news/800a/2021/cat-1.jpg" alt="жіноча білизна"/>
+                                    </li>
+                                    <li className="color-list-item">
+                                        <img className="product-image" src="https://scx1.b-cdn.net/csz/news/800a/2021/cat-1.jpg" alt="жіноча білизна"/>
+                                    </li>
+                                    <li className="color-list-item">
+                                        <img className="product-image" src="https://scx1.b-cdn.net/csz/news/800a/2021/cat-1.jpg" alt="жіноча білизна"/>
+                                    </li>
+                                </ul>
+                            </div>
                         </li>
                         <li className="product-list-item">                          
                             <p className="product-name">
@@ -26,6 +47,18 @@ const ProductScreen = () => {
                             </p>                            
                             <p className="product-name">
                                 Наявність: <span className="span-text">є в наявності</span>
+                            </p>
+                            <p className="product-name">
+                                Розмір
+                                <select className="product-size">
+                                    <option value="XS">XS</option>
+                                    <option value="S">S</option>
+                                    <option value="L">L</option>
+                                    <option value="XL">XL</option>
+                                    <option value="XXL">XXL</option>
+                                    <option value="Batal">Batal</option>
+                                </select>
+                                <Link to="/size" className="navbar-links" target="_blank">Таблиця розмірів</Link>
                             </p>
                             <p className="product-name-qty">
                                 Кількість
