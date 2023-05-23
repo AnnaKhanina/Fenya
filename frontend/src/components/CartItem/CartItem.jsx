b0cf39ed6d06064ff41657efceb99e1180e6e296
Anna Khanina <77342940+AnnaKhanina@users.noreply.github.com>
2023-05-23 17:21:16 +0300

    fix

--- frontend/src/components/CartItem/CartItem.jsx
+++ frontend/src/components/CartItem/CartItem.jsx
@@ -2,18 +2,12 @@ import { Link } from "react-router-dom";
 import { IconContext } from "react-icons";
 import * as FaIcons from "react-icons/fa"; 
 import "../CartItem/CartItem.css";
-import "../../screens/MainScreen/MainScreen.css";
-import "../Container/Container.css";
 
 const CartItem = () => {
-    return (
-        <main className="main-screen">
-            <IconContext.Provider value={{ color: "#060b26" }}> 
-            <div className="container">
-                <div className="cartitem">
-                    <div className="cartitem-image">
-                        <img src="https://scx1.b-cdn.net/csz/news/800a/2021/cat-1.jpg" alt ="жіноча білизна"/>
-                    </div>
+    return (                      
+            <div className="cartitem">
+                <IconContext.Provider value={{ color: "#060b26" }}>                   
+                    <img className="cartitem-image img" src="https://scx1.b-cdn.net/csz/news/800a/2021/cat-1.jpg" alt ="жіноча білизна"/>                
                     <Link to={`/product/${111}`} className="cartitem-name">
                         <p>Трусики жіночі Anna</p>
                     </Link>
@@ -27,10 +21,8 @@ const CartItem = () => {
                     <button className="cartitem-deleteBtn">
                         <span className="cartitem-icon"><FaIcons.FaTrash /></span>
                     </button>
-                </div>
-            </div>
-            </IconContext.Provider>          
-        </main>
+                </IconContext.Provider>
+            </div>    
     );
 };
 
