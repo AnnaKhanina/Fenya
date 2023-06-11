8e28ac38d1ae11ab66704b7ba45d8e5190671d70
Anna Khanina <77342940+AnnaKhanina@users.noreply.github.com>
2023-06-11 16:59:06 +0300

    +

--- backend/setupProxy.js
+++ backend/setupProxy.js
@@ -4,8 +4,20 @@
 //   app.use(
 //     '/api',
 //     createProxyMiddleware({
-//       target: 'http://localhost:3000',
+//       target: 'http://localhost:3000/',
 //       changeOrigin: true,
 //     })
 //   );
 // };
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+const testProxy = createProxyMiddleware ({
+  target: 'http://localhost:3000/',
+  changeOrigin: true,
+});
+
+app.use('/api', testProxy);
+app.listen(3000);
